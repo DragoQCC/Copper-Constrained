ec1533ad57a2908dd843d3fa7c912f77be0f53d3
Jonathan Owens <bumblebeejon@gmail.com>
2022-03-16 17:26:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace Copper_Constrained
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UI.Ui.Banner();
+            NoCLM();
+        }
+        
+        
+        public static void NoCLM()
+        {
+            bool OutString = true;
+            //create a powershell object
+            var ps = PowerShell.Create();
+            //create a new runspace
+            ps.Runspace = RunspaceFactory.CreateRunspace();
+            //open the runspace
+            ps.Runspace.Open();
+            //set the powershell object to run the user input
+            Console.WriteLine("Enter your command: ");
+            while (true)
+            {
+                Console.Write("Copper > ");
+                string input = Console.ReadLine();
+                if (input == "exit")
+                {
+                    ps.Runspace.Close();
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    //adds command and checks to the powershell object to get a good verbose output from the command
+                    ps.AddScript(input);
+                    if (OutString) { ps.AddCommand("Out-String"); }
+                    PSDataCollection<object> results = new PSDataCollection<object>();
+                    ps.Streams.Error.DataAdded += (sender, e) =>
+                    {
+                        Console.WriteLine("Error");
+                        foreach (ErrorRecord er in ps.Streams.Error.ReadAll())
+                        {
+                            results.Add(er);
+                        }
+                    };
+                    ps.Streams.Verbose.DataAdded += (sender, e) =>
+                    {
+                        foreach (VerboseRecord vr in ps.Streams.Verbose.ReadAll())
+                        {
+                            results.Add(vr);
+                        }
+                    };
+                    ps.Streams.Debug.DataAdded += (sender, e) =>
+                    {
+                        foreach (DebugRecord dr in ps.Streams.Debug.ReadAll())
+                        {
+                            results.Add(dr);
+                        }
+                    };
+                    ps.Streams.Warning.DataAdded += (sender, e) =>
+                    {
+                        foreach (WarningRecord wr in ps.Streams.Warning)
+                        {
+                            results.Add(wr);
+                        }
+                    };
+                    ps.Invoke(null, results);
+                    string output = string.Join(Environment.NewLine, results.Select(R => R.ToString()).ToArray());
+                    ps.Commands.Clear();
+                    Console.WriteLine(output);
+                }
+            }
+        }
+    }
+}
---
+++ UI/Ui.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pastel;
+
+namespace Copper_Constrained.UI
+{
+    public class Ui
+    {
+        public static void Banner()
+        {
+            Console.WriteLine(
+              " ██████╗ ██████╗ ██████╗ ██████╗ ███████╗██████╗      ██████╗ ██████╗ ███╗   ██╗███████╗████████╗██████╗  █████╗ ██╗███╗   ██╗███████╗██████╗\n".Pastel(Color.FromArgb(184, 115, 51)) +
+              "██╔════╝██╔═══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗    ██╔════╝██╔═══██╗████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██║████╗  ██║██╔════╝██╔══██╗\n".Pastel(Color.FromArgb(184, 115, 51)) +
+              "██║     ██║   ██║██████╔╝██████╔╝█████╗  ██████╔╝    ██║     ██║   ██║██╔██╗ ██║███████╗   ██║   ██████╔╝███████║██║██╔██╗ ██║█████╗  ██║  ██║\n".Pastel(Color.FromArgb(184, 115, 51)) +
+              "██║     ██║   ██║██╔═══╝ ██╔═══╝ ██╔══╝  ██╔══██╗    ██║     ██║   ██║██║╚██╗██║╚════██║   ██║   ██╔══██╗██╔══██║██║██║╚██╗██║██╔══╝  ██║  ██║\n".Pastel(Color.FromArgb(184, 115, 51)) +
+              "╚██████╗╚██████╔╝██║     ██║     ███████╗██║  ██║    ╚██████╗╚██████╔╝██║ ╚████║███████║   ██║   ██║  ██║██║  ██║██║██║ ╚████║███████╗██████╔╝\n".Pastel(Color.FromArgb(184, 115, 51)) +
+              " ╚═════╝ ╚═════╝ ╚═╝     ╚═╝     ╚══════╝╚═╝  ╚═╝     ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝╚═════╝ \n".Pastel(Color.FromArgb(184, 115, 51)) +
+                "\n" +
+               "Author: Jon @QueenCityCyber \n".Pastel(Color.SkyBlue) +
+               "https://github.com/Queen-City-Cyber \n".Pastel(Color.FromArgb(52, 152, 52))
+                );
+        }
+    }
+}
